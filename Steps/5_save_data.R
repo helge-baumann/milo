942c0475bba4470c4d92e1f8fd1358005d85ad5b
helge-baumann <helge-baumann@boeckler.de>
2021-03-27 14:20:43 +0100

    deskriptiv

--- Steps/5_save_data.R
+++ Steps/5_save_data.R
@@ -3,4 +3,7 @@
 dir.create("./Output/Daten", showWarnings=F)
 write_dta(evs13, paste0("./Output/Daten/", Sys.Date(), "_EVS2013.dta"))
 write_dta(evs18, paste0("./Output/Daten/", Sys.Date(), "_EVS2018.dta"))
+
+# save Workspace
+save.image(file=paste0("./Output/WS_", Sys.Date(), ".RData"))
           
