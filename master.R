6a5fbe975bdbdbac65d1132220380bf2297a079f
helge-baumann <helge-baumann@boeckler.de>
2021-03-27 13:46:52 +0100

    milo

---
+++ master.R
@@ -0,0 +1,27 @@
+####################################
+# Mindestlohnevaluation: Analyse der EVS-Daten
+# Helge Emmler
+# R-Version: 4.0.3
+# Letztes Update: 27.03.2021
+####################################
+
+# Präambel
+if (!("pacman" %in% installed.packages()[, 1])) install.packages("pacman")
+library(pacman)
+p_load(haven, dplyr, stringr, tidyr, ggplot2, gridExtra, kableExtra)
+
+# Subdateien ausführen
+source("./functions/functions.R", encoding="UTF-8")
+
+n <- 1:length(dir("./Steps"))
+sapply(dir("./Steps", full.names=T)[n], source, encoding="UTF-8")
+
+# Session Info----
+dir.create("./session_Info/", showWarnings=F)
+writeLines(
+  capture.output(sessionInfo()), 
+  con=paste0("./Session_Info/sessionInfo_", format(Sys.time(), "%y%m%d"), ".txt")
+)
+
+
+
